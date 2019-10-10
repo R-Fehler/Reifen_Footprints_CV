50cb2d04ccf9ea0a472b46e648114513aa0f00df
Richard Fehler <timblezero@gmail.com>
2019-10-10 18:26:56 +0200

    relative ROIS

--- find_cross_debug.m
+++ find_cross_debug.m
@@ -2,12 +2,18 @@ clearvars;
 close all hidden;
 clc;
 
+margin_top=500;
+margin_bottom=500;
+margin_left=30;
+margin_right=20;
 
+img_a=imread(fullfile("C:\footprint_auswertung_druckmessfolien\FootprintsJan\20191004_Scans_Loop-3\Original\Ac1s2Asphalt2.6\22.jpg"));
+figure,imshow(img_a);
+[img_height,img_width,rgb_dim]=size(img_a);
 
-img_a=imread(fullfile(pwd,'Footprints_Julien\Original\AC11DS_Sommer\3.jpg'));
- figure,imshow(img_a);
-left_cross_ROI=[50 500 1300 5000];
-right_cross_ROI=[3500 500 1400 5000];
+left_cross_ROI=[margin_left margin_top 0.5*img_width img_height-margin_top-margin_bottom];
+right_cross_ROI=[0.5*img_width margin_top 0.5*img_width-margin_right img_height-margin_top-margin_bottom ];
+
 
 [xx,yy]=find_cross_xy(img_a,left_cross_ROI);
 [x2,y2]=find_cross_xy(img_a,right_cross_ROI);
